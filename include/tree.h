738279a5c7b980d444ae12f6e0c712b148f8e745
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-06-08 21:00:39 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,64 +1,68 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <algorithm>
 #include <vector>
 #include <string>
+
+struct Node {
+    char symbol;
+    Node *parent;
+    std::vector<Node> variants;
+};
+
 class Tree {
  private:
-  struct Node {
-    char value;
-    std::vector<Node*> sheet;
-  };
-  Node* root;
-  std::vector<std::string> permutation;
-  void constructTree(Node* root, std::vector<char> figure) {
-    if (!figure.size()) {
-      return;
+    Node *root = new Node;
+    std::vector<Node> permutations;
+    void makeTree(std::vector<char> array) {
+        createNode(root, array);
+        link(root);
     }
-    if (root->value != '*') {
-      for (auto x = figure.begin(); x != figure.end(); ++x) {
-        if (*x == root->value) {
-          figure.erase(x);
-          break;
+    void createNode(Node* head, std::vector<char> array) {
+        for (int i = 0; i < array.size(); i++) {
+            Node *elem = new Node();
+            elem->symbol = array[i];
+            head->variants.push_back(*elem);
+            if (array.size() > 0) {
+                createNode(&head->variants[i], expectArray(array, array[i]));
+            }
         }
-      }
     }
-    for (size_t i = 0; i < figure.size(); ++i) {
-      root->sheet.push_back(new Node);
+    void link(Node *head) {
+        for (Node &child : head->variants) {
+            child.parent = head;
+            link(&child);
+        }
+        if (head->variants.size() == 0) {
+            permutations.push_back(*head);
+        }
     }
-    for (size_t i = 0; i < root->sheet.size(); ++i) {
-      root->sheet[i] = figure[i];
-      root->sheet[i]->value = figure[i];
+    std::vector<char> expectArray(std::vector<char> array, char value) {
+        std::vector<char>::iterator position = std::find(array.begin(),
+                                                         array.end(), value);
+        if (position != array.end())
+            array.erase(position);
+        return array;
     }
-    for (size_t i = 0; i < root->sheet.size(); ++i) {
-      constructTree(root->sheet[i], figure);
-    }
-  }
-  void Permutation(Node*root, std::string s = "") {
-    if (!root->sheet.size()) {
-      s += root->value;
-      permutation.push_back(s);
-      return;
-    }
-    if (root->value != '*') {
-      s += root->value;
-    }
-    for (size_t i = 0; i < root->sheet.size(); ++i) {
-      Permutation(root->sheet[i], s);
-    }
-  }
+
  public:
-  explicit Tree(std::vector<char> v) {
-    root = new Node;
-    root->value = '*';
-    constructTree(root, v);
-    Permutation(root);
-  }
-  std::string operator[](int i) const {
-    if (i >= permutation.size()) {
-      return "";
+    explicit Tree(std::vector<char> array) {
+        makeTree(array);
     }
-    return permutation[i];
-  }
+    std::vector<char> getPByN(Tree tree, int number){
+        if (number > permutations.size()) {
+            return {};
+        }
+        std::vector<char> result;
+        Node current = permutations[number-1];
+        while (current.parent) {
+            result.push_back(current.symbol);
+            current = *current.parent;
+        }
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
 };
+
 #endif  // INCLUDE_TREE_H_
